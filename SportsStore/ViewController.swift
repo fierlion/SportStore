98df4cc541d9bc9273e9ab72647983bda53de7f9
fierlion <fierlion@gmail.com>
2015-12-13 18:44:49 -0800

    updating count in view

--- SportsStore/ViewController.swift
+++ SportsStore/ViewController.swift
@@ -9,17 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //MARK: Properties
+    
+    @IBOutlet weak var totalStockLabel: UILabel!
+    
+    
+    var products = [
+        ("Kayak", "a boat for one person", "watersports", 275.0, 10),
+        ("Soccer Ball", "fifa approved size and weight", "soccer", 19.5, 32),
+        ("stadium", "35000 seat stadium", "soccer", 79500.0, 4),
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        displayStockTotal()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func displayStockTotal() {
+        let stockTotal = products.reduce(0, combine: {(total, product) -> Int in return total + product.4})
+        totalStockLabel.text = "\(stockTotal) Products in stock"
+    }
 }
-
