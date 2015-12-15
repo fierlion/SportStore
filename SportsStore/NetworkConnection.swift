d109f46c01894e162c67447a4d4219922e49a6c0
fierlion <fierlion@gmail.com>
2015-12-15 12:45:21 -0800

    added network sim

---
+++ SportsStore/NetworkConnection.swift
@@ -0,0 +1,28 @@
+//
+//  NetworkConnection.swift
+//  SportsStore
+//
+//  Created by Ray Foote on 12/15/15.
+//  Copyright © 2015 fierlion. All rights reserved.
+//
+
+import Foundation
+
+class NetworkConnection {
+    
+    private let stockData: [String: Int] = [
+        "kayak": 10, "Lifejacket": 4, "soccer ball": 32
+    ]
+    
+    func getStockLevel(name:String) -> Int? {
+        NSThread.sleepForTimeInterval(Double(rand() % 2))
+        if let result = stockData[name] {
+            return result
+        }
+        else {
+            return 0
+        }
+    }
+
+
+}
